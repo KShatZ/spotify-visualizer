27d29e6678ecb3a825713de97b99c055b30e484c
KShatZ <kirillshats@gmail.com>
2023-09-06 11:31:10 -0400

    User class for flask_login authentication

---
+++ spotify_visualizer/models/user.py
@@ -0,0 +1,20 @@
+import os
+
+from bson import ObjectId
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from flask_login import UserMixin
+
+from pymongo import MongoClient
+
+
+
+class User(UserMixin):
+
+    def __init__(self, user_doc):
+        self.id = user_doc["_id"]
+        self.username = user_doc["username"]
+        self.spotify = user_doc["spotify"] # Remove the token values eventually
+
