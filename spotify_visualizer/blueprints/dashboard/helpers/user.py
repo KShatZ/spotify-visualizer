5b6885d13be3825be201d5bec44a48fced9042f1
KShatZ <kirillshats@gmail.com>
2023-09-07 13:58:18 -0400

    Docstring update

--- spotify_visualizer/blueprints/dashboard/helpers/user.py
+++ spotify_visualizer/blueprints/dashboard/helpers/user.py
@@ -5,9 +5,9 @@ import requests
 
 from spotify_visualizer.blueprints.spotify.helpers.spotify import get_access_token, refresh_access_token
 
-
 mongo = MongoClient()
 USERS_COLLECTION =  mongo["spotify-visualizer"]["users"]
+
 
 def get_user_info(user_id):
     """Queries users collection to get the spotify.user object
@@ -29,6 +29,8 @@ def get_users_total_tracks(app, user_id):
     """Sends a request to a spotify endpoint and returns the total
     number of tracks the current user has saved.
 
+    :param app: The current flask app - for loggging
+    :type app: Flask
     :param user_id: _id of current user
     :type user_id: str
     :return: Total number of songs the user has saved
