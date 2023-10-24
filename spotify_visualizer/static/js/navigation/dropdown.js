f2d611e4bd5d6931f12375e3028620481a6ab5aa
KShatZ <kirillshats@gmail.com>
2023-10-24 14:53:58 -0400

    Toggle logout functionality in nav

---
+++ spotify_visualizer/static/js/navigation/dropdown.js
@@ -0,0 +1,15 @@
+const dropdownToggle = document.getElementById("dropdown-toggle");
+const dropdownArrow = document.getElementById("dropdown-arrow");
+const navDropdown = document.getElementById("nav-dropdown");
+
+dropdownToggle.addEventListener("click", () => {
+
+    if (navDropdown.style.display ==  "none") {
+        navDropdown.style.display = "block";
+        dropdownArrow.classList.add("accent")
+    } else {
+        navDropdown.style.display = "none";
+        dropdownArrow.classList.remove("accent");
+    }
+
+});
