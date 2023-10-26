43e38257cc18e719b18bd608db051d518875a7c2
KShatZ <kirillshats@gmail.com>
2023-10-26 11:47:37 -0400

    Spotify vars

---
+++ docker/environment.sh
@@ -0,0 +1,4 @@
+# SPOTIFY oAuth
+export SPOTIFY_CLIENT_ID="Your Spotify App Client ID"
+export SPOTIFY_CLIENT_SECRET="Your Spotify App Client Secret"
+export SPOTIFY_REDIRECT_URI="URL To Redirect After Spotify oAUTH"
