e44b7c4a03978382d2bb6b738b91c8afddd56346
KShatZ <kirillshats@gmail.com>
2023-11-04 14:55:36 -0400

    Prod network config

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 if [ "$FLASK_APP_MODE" == "PROD" ]; then
     echo "[Start Up] Starting Gunicorn, port 8000..."
-    gunicorn app:app -w 3 --bind 127.0.0.1:8000 --access-logfile - --error-logfile -
+    gunicorn app:app -w 3 --bind 0.0.0.0:8000 --access-logfile - --error-logfile -
 elif [ "$FLASK_APP_MODE" == "DEV" ]; then
     echo "[Start Up] Dev mode..."
     tail -f /dev/null
