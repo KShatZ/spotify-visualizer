958419405897251b0a39871f607668b17ce05ed8
KShatZ <kirillshats@gmail.com>
2024-03-29 13:47:34 -0400

    React router and auth form changes

--- client/src/components/authentication/Form.jsx
+++ client/src/components/authentication/Form.jsx
@@ -1,17 +1,90 @@
-import Input from "./Input";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 import "./styles.css";
 
 export default function Form({ isLogin }) {
 
+    const [ username, setUsername ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+    function handleInput(event) {
+        const inputName = event.target.name;
+        let inputValue = event.target.value;
+
+        switch (inputName) {
+            case "username":
+                setUsername(inputValue);
+                break;
+            case "password":
+                inputValue = inputValue.replace(/./g, "♫"); // Maybe??
+                setPassword(inputValue);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        // Todo: Better input validation
+        if (username.length == 0 || password.length == 0) {
+            console.log("The username and password fields cannot be empty");
+            return;
+        }
+
+        const userCredentials = {
+            "username": username,
+            "password": password
+        }
+
+        const endpoint = isLogin ? "login" : "register"; 
+
+        // Todo: Create own request interface
+        const response = await fetch(`http://127.0.0.1:5000/${endpoint}`, {
+            method: "POST",
+            body: JSON.stringify(userCredentials),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        console.log("The RESPONSE: ", response);
+            
+        const responseBody = await response.json();
+        console.log("Response BODY: ", responseBody);
+    }
+
+
     return (
         <div id="auth-form-container" className="container">
-            <form id="auth-form" className="grey-border-2" action="/login" method="POST">
+            <form id="auth-form" className="grey-border-2" onSubmit={ handleSubmit }>
 
                 <h1>{ isLogin ? "Login:":"Register:" }</h1>
 
                 <div id="auth-form-inputs">
-                    <Input type="username" />
-                    <Input type="password" />
+                    <div id="auth-input-group">
+                        <label htmlFor="username">Username:</label>
+                        <input className="bg-color txt-color grey-border-2"
+                            id="username"
+                            name="username"
+                            value={ username }
+                            type="text"
+                            onChange={ handleInput }
+                        />
+                    </div>
+
+                    <div id="auth-input-group">
+                        <label htmlFor="password">Password:</label>
+                        <input className="bg-color txt-color grey-border-2"
+                            id="password"
+                            name="password"
+                            value={ password }
+                            type="text"
+                            onChange={ handleInput }
+                        />
+                    </div>
                 </div>
 
                 {/* <p id="form-error"></p> */}
@@ -21,12 +94,12 @@ export default function Form({ isLogin }) {
                 { isLogin ? 
                     <div id="auth-redirect">
                         <p>Dont have an account?</p>
-                        <a href="/register">Create Account</a>
+                        <Link to="/register">Create Account</Link>
                     </div>
                     :
                     <div id="auth-redirect">
                         <p>Already have an account?</p>
-                        <a href="/login">Login</a>
+                        <Link to="/login">Login</Link>
                     </div>
                 }
             </form>
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles.css";
 
+import Protected from "./routes/Protected";
 import Login from "./routes/Login";
 import Register from "./routes/Register";
+import Dashboard from "./routes/Dashboard";
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Protected />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />
+      },
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login />
+    <RouterProvider router={ router } />
   </React.StrictMode>,
 )
