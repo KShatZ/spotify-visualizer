e25af9ba3cd3ba1e1e5d94ffc68da53b1f503086
KShatZ <kirillshats@gmail.com>
2024-04-12 12:23:54 -0400

    Register page functionality

--- client/src/components/authentication/Form.jsx
+++ client/src/components/authentication/Form.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 import "./styles.css";
 
@@ -7,24 +7,50 @@ export default function Form({ isLogin }) {
 
     const [ username, setUsername ] = useState("");
     const [ password, setPassword ] = useState("");
+    const [ formMessage, setFormMessage ] = useState(null);
 
+    const navigate = useNavigate();
+    let location = useLocation();
+
+    const classes = {
+        default: "grey-border-2",
+        message: "form-message grey-border-2"
+    }
+
+    useEffect(() => {
+        if (isLogin && location.state) {
+            if (location.state.userCreated) {
+                /** After succesful account creation user is redirected to login
+                 * page. Need to show success message and prompt login.
+                 */
+                if (!formMessage) {
+                    setFormMessage({
+                        error: false,
+                        msg: "Account succesfully created, you may now login!"
+                    });
+                } else {
+                    // Clear userCreated state after message shown
+                    delete location.state.userCreated;
+                }
+            }
+        }
+    }, [isLogin, location, formMessage])
+    
     function handleInput(event) {
         const inputName = event.target.name;
         let inputValue = event.target.value;
-
+   
         switch (inputName) {
             case "username":
                 setUsername(inputValue);
                 break;
             case "password":
-                inputValue = inputValue.replace(/./g, "♫"); // Maybe??
-                setPassword(inputValue);
-                break;
+                setPassword(inputValue)
+                break; 
             default:
                 break;
         }
     }
-
 
     async function handleSubmit(event) {
         event.preventDefault();
@@ -36,30 +62,101 @@ export default function Form({ isLogin }) {
         }
 
         const userCredentials = {
-            "username": username,
-            "password": password
+            username: username,
+            password: password
         }
 
         const endpoint = isLogin ? "login" : "register"; 
 
         // Todo: Create own request interface
-        const response = await fetch(`http://127.0.0.1:5000/${endpoint}`, {
-            method: "POST",
-            body: JSON.stringify(userCredentials),
-            headers: {
-                "Content-Type": "application/json"
+        try {
+            const response = await fetch(`http://127.0.0.1:5000/${endpoint}`, {
+                method: "POST",
+                body: JSON.stringify(userCredentials),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+
+            const body = await response.json();
+
+            if (endpoint == "login") { 
+                // TODO
+            } else {
+                switch(response.status) {
+
+                    // User created succesfully
+                    case 201: 
+                        navigate("/login", {
+                            state: {
+                                userCreated: true
+                            }
+                        });
+                        break; 
+                    
+                    // User provided invalid input
+                    case 400:
+                        setFormMessage({
+                            error: true,
+                            msg: body.error.msg
+                        });
+                        break;
+
+                    // User already exists - eventually remove
+                    case 409: 
+                        setFormMessage({
+                            error: true,
+                            msg: body.error.msg
+                        });
+                        break;
+
+                    // Some server error
+                    case 500:
+                        setFormMessage({
+                            error: true,
+                            msg: body.error.msg
+                        });  
+                        break;
+                    default:
+                        break;
+                }
             }
-        });
-        console.log("The RESPONSE: ", response);
-            
-        const responseBody = await response.json();
-        console.log("Response BODY: ", responseBody);
+        } catch(e) {
+
+            let message;
+            if (endpoint == "login") {
+                message = "There was an issue logging you in on our end, please try again later.";
+            } else {
+                message = "There was an issue creating your account on our end, please try again later.";
+            }
+
+            setFormMessage({
+                error: true,
+                msg: message
+            });
+        }
     }
+
+    // Register request:
+    //  - 201: User created
+    //  - 400: Invalid Input
+    //  - 409: User already exists
+    //  - 500: Some server error
+
+    // Login request:
+    //  - 200: User logged in
+    //  - 400: Bad username and/or password
+    //  - 401: Need to setup Spotify oAuth
+    //  - 500: Some issue logging in
 
 
     return (
         <div id="auth-form-container" className="container">
-            <form id="auth-form" className="grey-border-2" onSubmit={ handleSubmit }>
+            <form 
+                id="auth-form" 
+                className={ formMessage ? classes.message : classes.default }
+                onSubmit={ handleSubmit }
+            >
 
                 <h1>{ isLogin ? "Login:":"Register:" }</h1>
 
@@ -81,13 +178,17 @@ export default function Form({ isLogin }) {
                             id="password"
                             name="password"
                             value={ password }
-                            type="text"
+                            type="password"
                             onChange={ handleInput }
                         />
                     </div>
                 </div>
 
-                {/* <p id="form-error"></p> */}
+                { formMessage && 
+                    <p id="auth-form-msg" className={ formMessage.error ? "error-msg" : "success-msg" }>
+                        { formMessage.msg }
+                    </p> 
+                }
 
                 <button type="submit" style={ isLogin ? {}:{width:"65%"} }>{ isLogin ? "Login":"Create Account" }</button>
 
