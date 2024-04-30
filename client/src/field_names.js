5256864bd26ce710bd57790eec0fc609c6d6c7e2
KShatZ <kirillshats@gmail.com>
2024-04-30 15:06:25 -0400

    HTTP Status Code field names

---
+++ client/src/field_names.js
@@ -0,0 +1,14 @@
+const HTTP = {
+    OK: "200",
+    CREATED: "201",
+
+    SEE_OTHER: "303",
+
+    BAD_REQUEST: "400",
+    UNAUTHORIZED: "401",
+    CONFLICT: "409",
+
+    SERVER_ERROR: "500"
+}
+
+export default HTTP;
