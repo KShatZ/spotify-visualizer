3cad35a2f84b65b565ccc56716d72620c351e0bf
KShatZ <kirillshats@gmail.com>
2024-04-30 15:08:30 -0400

    Conditional rendering based on request authentication status

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -1,12 +1,20 @@
+import { Navigate, useLoaderData} from "react-router-dom";
+
 import Header from "../components/authentication/Header";
 import Form from "../components/authentication/Form";
 
+
 export default function Login() {
 
-    return (
-    <>
-        <Header />
-        <Form isLogin={true} />
-    </>
+    const isAuthenticated = useLoaderData();
+
+    return (isAuthenticated ?
+        
+        <Navigate to="/" />
+        :
+        <>
+            <Header />
+            <Form isLogin={true} />
+        </>
     )
 }
