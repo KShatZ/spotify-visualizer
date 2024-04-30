4f13eb1b9ee9aab381a80b65d560f59c8569fb24
KShatZ <kirillshats@gmail.com>
2024-04-30 15:04:33 -0400

    Configure proxy server for dev stack, avoid CORS.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // To avoid ÇORS in development
+  server: {
+    proxy: {
+      "/api": {
+        target: 'http://localhost:5000', // Set to backend server
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
