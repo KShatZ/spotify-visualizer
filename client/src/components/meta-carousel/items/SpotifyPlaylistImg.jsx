7ffa85ab97df94a34604d162a376f9a32b9a83f3
KShatZ <kirillshats@gmail.com>
2024-05-13 18:23:18 -0400

    Spotify Playlist Image Carousel Item

--- client/src/components/meta-carousel/items/SpotifyPlaylistImg.jsx
+++ client/src/components/meta-carousel/items/SpotifyPlaylistImg.jsx
@@ -1,3 +1,15 @@
-export default function SpotifyPlaylistImg() {
+export default function SpotifyPlaylistImg({ img, onTouchStart, onTouchMove, onTouchEnd }) {
     
+    return (
+        <div 
+            id="meta-img-container" 
+            className="carousel-item-container exit-left grey-border-2"
+            onTouchStart={onTouchStart}
+            onTouchMove={onTouchMove}
+            onTouchEnd={onTouchEnd}
+        >
+            <img src={img} alt="Spotify Playlist Image" />
+        </div>
+    )
+
 }
